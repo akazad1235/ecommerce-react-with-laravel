c0f351f4c4e4b402bc3bdd56e364dc968a32881b
akazad1235 <akazad914@gmail.com>
2021-03-21 20:50:07 +0600

    footer copyright section added

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -3,7 +3,8 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <div className="bg">
+        <>
+            <div className="bg">
             <Container>
                 <Row className="mt-5 pt-5 pb-5">
                     <Col lg={3} md={6} sm={6} xm={4}>
@@ -17,16 +18,31 @@ const Footer = () => {
                     </Col>
                     <Col lg={3} md={6} sm={6} xm={4}>
                         <h4>The Company</h4>
+                        <li className=""><a href="">About us</a></li>
+                        <li className=""><a href="">Contact us</a></li>
+                        <li className=""> <a href="">Our Team</a></li>
+                        <h6>Address</h6>
+                        <p>268/A, Middle Pirerbag, Mirpur-1, Dhaka-1205</p>
                     </Col>
                     <Col lg={3} md={6} sm={6} xm={4}>
                         <h4 className="mb-3">More Info</h4>
+                        <li className=""><a href="">How to Product Purchase</a></li>
+                        <li className=""><a href="">Privacy Policy</a></li>
+                        <li className=""> <a href="">Refund Policy</a></li>
                     </Col>
                     <Col lg={3} md={6} sm={6} xm={4}>
                         <h4 className="mb-3">Download App</h4>
+
                     </Col>
                 </Row>
             </Container>
         </div>
+        <div className="footer-copyright-bg">
+            <Container>
+            <p>	&#169; copyright 2020 by ecome express</p>
+            </Container>
+        </div>
+        </>
     );
 };
 
